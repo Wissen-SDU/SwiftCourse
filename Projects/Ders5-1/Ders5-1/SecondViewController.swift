6319a0779c5131fabeaa6bbbe089e701e699145b
Suleyman Calik <suleymancalik@gmail.com>
2014-11-28 15:01:53 +0200

    secondvc

--- Projects/Ders5-1/Ders5-1/SecondViewController.swift
+++ Projects/Ders5-1/Ders5-1/SecondViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import Realm
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITableViewDataSource {
 
+    var users:RLMResults!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        users = User.allObjects()
+        
+        var tableView = UITableView(frame: view.bounds, style: UITableViewStyle.Plain)
+        tableView.dataSource = self
+        
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "UserCell")
+        
+        view.addSubview(tableView)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    // MARK: TableView Methods
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Int(users.count)
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier("UserCell") as UITableViewCell
+        var user:User = users[UInt(indexPath.row)] as User
+        cell.textLabel.text = user.name
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        let realm = RLMRealm.defaultRealm()
+        realm.beginWriteTransaction()
+        
+        var user = users[UInt(indexPath.row)] as User
+        realm.deleteObject(user)
+        
+        realm.commitWriteTransaction()
+
+        tableView.reloadData()
+    }
 
 }
+
+
+
+
+
 
