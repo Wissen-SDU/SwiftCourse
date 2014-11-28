1dce6c985fd4f00f82d04195330b299f882591ab
Suleyman Calik <suleymancalik@gmail.com>
2014-11-28 15:33:09 +0200

    Arayuz duzenlemesi

--- Projects/Ders5-1/Ders5-1/FirstViewController.swift
+++ Projects/Ders5-1/Ders5-1/FirstViewController.swift
@@ -9,20 +9,32 @@
 import UIKit
 import Realm
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController , UITextFieldDelegate {
 
     @IBOutlet weak var txtUsername: UITextField!
     @IBOutlet weak var lblMessage: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        txtUsername.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    @IBAction func backgroundTapped(sender: AnyObject) {
+        txtUsername.resignFirstResponder()
+    }
+    
 
     @IBAction func getirTapped(sender: UIButton) {
         
